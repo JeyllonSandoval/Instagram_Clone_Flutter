2d156a5041021737fab5fe76492314d0f922bc41
JeyllonSandoval <jeyllomsandoval@gmail.com>
2024-11-25 21:40:57 -0400

    feat: menu navigation

--- lib/auth/mainpage.dart
+++ lib/auth/mainpage.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_flutter/auth/auth_screen.dart';
-import 'package:instagram_clone_flutter/screen/home.dart';
+import 'package:instagram_clone_flutter/widget/navigation.dart';
+
 
 
 class MainPage extends StatelessWidget {
@@ -14,7 +15,7 @@ class MainPage extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const HomeScreen();
+            return const Navigations_Screen();
           } else {
             return const AuthScreen();
           }
---
+++ lib/screen/add_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class AddScreen extends StatefulWidget {
+  const AddScreen({super.key});
+
+  @override
+  State<AddScreen> createState() => _AddScreenState();
+}
+
+class _AddScreenState extends State<AddScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: const Text('Add Screen'),
+      ),
+    );
+  }
+}
