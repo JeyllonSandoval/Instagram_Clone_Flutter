70492ed69746dec072c5f42b7dbedb7a77d4c135
JeyllonSandoval <jeyllomsandoval@gmail.com>
2024-11-26 18:34:08 -0400

    feat: view post page

--- lib/screen/add_screen.dart
+++ lib/screen/add_screen.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:instagram_clone_flutter/screen/add_post_screen.dart';
+import 'package:instagram_clone_flutter/screen/add_reels_screen.dart';
 
 class AddScreen extends StatefulWidget {
   const AddScreen({super.key});
@@ -8,12 +10,97 @@ class AddScreen extends StatefulWidget {
   State<AddScreen> createState() => _AddScreenState();
 }
 
+int _currentIndex = 0;
+
 class _AddScreenState extends State<AddScreen> {
+  late PageController pageController;
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    pageController.dispose();
+  }
+
+  onPageChanged(int page) {
+    setState(() {
+      _currentIndex = page;
+    });
+  }
+
+  navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: const Text('Add Screen'),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            PageView(
+              controller: pageController,
+              onPageChanged: onPageChanged,
+              children: const [
+                AddPostScreen(),
+                AddReelsScreen(),
+              ],
+            ),
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 300),
+              bottom: 10.h,
+              right: _currentIndex == 0 ? 100.w : 150.w,
+              child: Container(
+                width: 120.w,
+                height: 30.h,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.6),
+                  borderRadius: BorderRadius.circular(20.r),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        navigationTapped(0);
+                      },
+                      child: Text(
+                        'Post',
+                        style: TextStyle(
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w500,
+                          color:
+                              _currentIndex == 0 ? Colors.white : Colors.grey,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        navigationTapped(1);
+                      },
+                      child: Text(
+                        'Reels',
+                        style: TextStyle(
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w500,
+                          color:
+                              _currentIndex == 1 ? Colors.white : Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
