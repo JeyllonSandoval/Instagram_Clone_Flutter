7a46566bb5a847fd9171262d50c3c0ce0a7631be
JeyllonSandoval <jeyllomsandoval@gmail.com>
2024-11-25 22:51:11 -0400

    feat: View posts in the feed

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:instagram_clone_flutter/widget/post_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +12,50 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: const Text('Home Screen'),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: SizedBox(
+          width: 105.w,
+          height: 28.h,
+          child: Image.asset('images/instagram.jpg'),
+        ),
+        leading: Image.asset('images/camera.jpg'),
+        actions: [
+          const Icon(
+            Icons.favorite_border_outlined,
+            color: Colors.black,
+            size: 25,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 10.w, left: 10.w), // Ajusta el espacio derecho.
+            child: Image.asset(
+              'images/messages.png',
+              width: 20.w,
+            ),
+          ),
+        ],
+        backgroundColor: const Color(0xffFAFAFA),
+      ),
+
+      body: CustomScrollView(
+        slivers: [
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return PostWidget();
+              },
+              childCount: 6,
+            ),
+          )
+        ],
       ),
     );
   }
